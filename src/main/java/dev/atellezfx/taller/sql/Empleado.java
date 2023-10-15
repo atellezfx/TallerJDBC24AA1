07d6a93b943b33f842ada493bb75cfa4c642255a
atellezf <alejandro@correo.com>
2023-10-14 20:08:08 -0600

    Clase Empleado con Lombok

--- src/main/java/dev/atellezfx/taller/sql/Empleado.java
+++ src/main/java/dev/atellezfx/taller/sql/Empleado.java
@@ -1,4 +1,14 @@
 package dev.atellezfx.taller.sql;
 
-public class Empleado {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Empleado { // Clase de entidad
+
+    private int id;
+    private String nombre, apellidos, email;
+    private float salario;
+
 }
--- src/main/java/dev/atellezfx/taller/sql/EmpleadoDAO.java
+++ src/main/java/dev/atellezfx/taller/sql/EmpleadoDAO.java
@@ -1,4 +1,13 @@
 package dev.atellezfx.taller.sql;
 
+import java.util.Optional;
+
 public class EmpleadoDAO {
+
+    public Optional<Empleado> obtener(int id) {
+        Empleado emp = null;
+
+        return Optional.ofNullable( emp );
+    }
+
 }
